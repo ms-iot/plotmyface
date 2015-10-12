f1c88bf8fe99ac731db084004ef7f0ba04e92afd
Lou Amadio <lou.amadio@gmail.com>
2015-10-12 12:11:30 -0700

    Plotbot
    Conflicts:
    	PlotMyFace/PlotMyFace/MainPage.xaml.cs

--- PlotMyFace/MachineInterface/HBot.h
+++ PlotMyFace/MachineInterface/HBot.h
@@ -1,42 +1,61 @@
 #pragma once
 #include "AccelStepper.h"
 
-ref class HBot sealed
+namespace MachineInterface
 {
-	typedef enum 
+	public ref class HBot sealed
 	{
-		BotState_Idle = 0,
-		BotState_Homing,
-		BotState_Moving
-	} BotState;
-	int _aStepPin; 
-	int _aDirPin;
-	int _aEnPin;
-	int _bStepPin; 
-	int _bDirPin;
-	int _bEnPin;
-	int _xHome;
-	int _yHome;
+		typedef enum
+		{
+			BotState_Idle = 0,
+			BotState_Homing,
+			BotState_Moving
+		} BotState;
+		int _aStepPin;
+		int _aDirPin;
+		int _aEnPin;
+		int _bStepPin;
+		int _bDirPin;
+		int _bEnPin;
+		int _xHome;
+		int _yHome;
 
-	uint32 _widthMM;
-	uint32 _heightMM;
-	uint32 _stepsPerMM;
+		uint32 _widthMM;
+		uint32 _heightMM;
+		uint32 _stepsPerMM;
 
-	BotState _state;
+		BotState _state;
 
-	AccelStepper _aStepper;
-	AccelStepper _bStepper;
-	void setMaxValues();
-public:
-	HBot(int aStepPin, int aDirPin, int aEnPin,
-		int bStepPin, int bDirPin, int bEnPin,
-		int xHome, int yHome);
-	void home();
-	void move(uint32 xMM, uint32 yMM);
-	void moveRelative(uint32 xMM, uint32 yMM);
-	void setInfo(
-		uint32 widthMM, uint32 heightMM,
-		uint32 stepsPerMM);
-	void run();
+		AccelStepper _aStepper;
+		AccelStepper _bStepper;
+		void setMaxValues();
+	public:
+		HBot(int aStepPin, int aDirPin, int aEnPin,
+			int bStepPin, int bDirPin, int bEnPin,
+			int xHome, int yHome);
+		void home();
+		void move(uint32 xMM, uint32 yMM);
+		void moveRelative(uint32 xMM, uint32 yMM);
+		void setInfo(
+			uint32 widthMM, uint32 heightMM,
+			uint32 stepsPerMM);
+		bool run();
+
+		bool atXStop()
+		{
+			return digitalRead(_xHome) == 1;
+		}
+
+		bool atYStop()
+		{
+			return digitalRead(_yHome) == 1;
+		}
+
+		bool isRunning()
+		{
+			return _state != BotState_Idle;
+		}
+	};
+
+
 };
-
